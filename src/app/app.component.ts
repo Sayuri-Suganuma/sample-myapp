65ea43f1cb0045f3b191e37a7661d0601f2a70ba
Sayuri.Suganuma <sayuri.suganuma@SayuriSuganumas-MacBook-Pro.local>
2024-04-09 10:17:25 +0900

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'sample-myapp';
+  title = 'URL';
+
+
+  constructor(private httpClient: HttpClient) { }
+
+  public async getHtmlContent(url: string): Promise<void> {
+    console.log(`TARGET URL = ${url}`);
+    return new Promise((resolve) => {
+      this.httpClient.get(url).subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+        error: (error) => {
+          console.log(error);
+        }
+      });
+    });
+  }
 }
